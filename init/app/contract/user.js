84ba859dfa35b8f101205106917e77c5d3210bda
qtvb1987 <qtvb1987@163.com>
2019-09-16 21:43:29 +0800

    代码整理

--- init/app/contract/user.js
+++ init/app/contract/user.js
@@ -1,7 +1,8 @@
-module.exports={
-    createUserRequest:{
-        mobile:{type:'string',required:true,description:'手机号',example:'13671839764',format:/^1[34578]\d{9}$/,},
-        password:{type:'string',required:true,description:'密码',example:'111111'},
-        realName:{type:'string',required:true,description:'姓名',example:'Tom'}
-    }
-}
+// eslint-disable-next-line strict
+module.exports = {
+  createUserRequest: {
+    mobile: { type: 'string', required: true, description: '手机号', example: '13671839764', format: /^1[34578]\d{9}$/ },
+    password: { type: 'string', required: true, description: '密码', example: '111111' },
+    realName: { type: 'string', required: true, description: '姓名', example: 'Tom' },
+  },
+};
--- init/app/controller/user.js
+++ init/app/controller/user.js
@@ -1,24 +1,25 @@
-const {Controller}=require('egg')
+// eslint-disable-next-line strict
+const { Controller } = require('egg');
 
 /**
  * @Controller 用户管理
  */
-class UserController extends Controller{
-    constructor(ctx){
-        super(ctx)
-    }
+class UserController extends Controller {
+  // eslint-disable-next-line no-useless-constructor
+  constructor(ctx) {
+    super(ctx);
+  }
 
-    /**
-     * @summary 创建用户
-     * @description 创建用户 记录用户账户信息
-     * @router post /api/user
-     * @request body createUserRequest *body
-     * @response 200 baseResponse 创建成功
-     */
-    async create()
-    {
-        const{ctx}=this
-        ctx.body='user ctrl'
-    }
+  /**
+   * @summary 创建用户
+   * @description 创建用户 记录用户账户信息
+   * @router post /api/user
+   * @request body createUserRequest *body
+   * @response 200 baseResponse 创建成功
+   */
+  async create() {
+    const { ctx } = this;
+    ctx.body = 'user ctrl';
+  }
 }
-module.exports=UserController
+module.exports = UserController;
